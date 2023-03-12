6aaed851f63df816f60ce7ef930e8f9c9993ddf8
Pepe <tururu.pompella@gmail.com>
2023-03-12 16:33:34 +0000

    Added bucketlist

--- win.py
+++ win.py
@@ -16,7 +16,10 @@ innerAngle = 0
 zero = time.time()
 frame = 50 #ms
 
-clear = "scan"
+def tick():
+    time.sleep(frame/1000)
+
+reset = "off"
 
 landmarks = []
 length = 8
@@ -29,10 +32,31 @@ for i in range(length - 1):
 for i in range(length - 1):
     landmarks.append([0, (i + 1)/2])
 
+tasks = {
+    "wheels" : ["panic()", 10**100, "wheels"],
+    "proximity" : ["front_prox_timeout()", 5, "proximity"],
+}
+
+def queue(task, sleep, tag, overwrite = False):
+    if overwrite or tasks[tag][1] == 10**100:
+        tasks[tag] = [task, time.time() - zero + sleep, tag]
+    return time.time() - zero + sleep
+
+def clear(tag):
+    tasks[tag] = ["print(yourmom)", 10**100, tag]
+
 frontProxTimeout = 5
 
 front_prox = False
 seen_front_prox = False
+
+def front_prox_timeout():
+    global front_prox, seen_front_prox
+
+    front_prox = False
+    seen_front_prox = False
+    queue("front_prox_timeout()", frontProxTimeout, "proximity")
+
 def front_prox_loop():
     global front_prox, seen_front_prox
     while True:
@@ -56,22 +80,12 @@ def check_front_prx():
     return front_prox
 
 
-tasks = {
-    "wheels" : ["panic()", 10**100, "wheels"],
-    "proximity" : ["front_prox_timeout()", 5, "proximity"]
-}
-
-def queue(task, sleep, tag, overwrite = False):
-    if overwrite or tasks[tag][1] == 10**100:
-        tasks[tag] = [task, time.time() - zero + sleep, tag]
-    
-
 def snap(x, y):
     x -= xy[0]
     y -= xy[1]
     dist = math.sqrt(x**2 + y**2)
     angle = math.degrees(math.asin(y/dist))
-    polar(dist, angle)
+    return polar(dist, angle)
 
 def polar(distance = 0, angle = 0, marker = 0):
     global xy, innerAngle
@@ -81,7 +95,7 @@ def polar(distance = 0, angle = 0, marker = 0):
         angle = marker.bearing.y
 
     turn(angle)
-    queue("move({})".format(distance), abs(angle/defAngle), "wheels", True)
+    return queue("move({})".format(distance), abs(angle/defAngle), "wheels", True)
 
 def move(dist):
     global xy
@@ -89,7 +103,8 @@ def move(dist):
     xy[0] += math.cos(innerAngle * dist)
     xy[1] += math.sin(innerAngle * dist)
     R.motors = [-100, -100, 0]
-    queue("{}()".format(clear), abs(dist/defDistance), "wheels")
+    queue("{}()".format(reset), abs(dist/defDistance), "wheels")
+    return abs(dist/defDistance)
 
 def turn(angle):
     global innerAngle
@@ -97,46 +112,21 @@ def turn(angle):
     sign = angle/abs(angle)
     R.motors = [100 * sign, -100 * sign, 0]
     innerAngle += angle
-    queue("{}()".format(clear), abs(angle/defAngle), "wheels")
+    queue("{}()".format(reset), abs(angle/defAngle), "wheels")
+    return abs(angle/defAngle)
 
-def check():
-    smallesttask = ["", None, ""]
-    for task in [*tasks.values()]:
-        if smallesttask[1] == None:
-            smallesttask = task
-            continue
-        if task[1] - time.time() + zero < 0 :
-            tasks[task[2]][1] = 10**100
-            continue
-        if task[1] < smallesttask[1]:
-                smallesttask = task
-    
-    if smallesttask[1] < time.time() - zero + frame:
-        time.sleep(smallesttask[1] - time.time() + zero)
-        eval(smallesttask[0])
-        check()
-
-def scan():
-    R.motors = [0, 0, 0]
-
-    R.see()
-    sheep = R.see()
-    
-    if sheep == []:
-        turn(60)
-        return
-
-    elif sheep[0].info.id in range(40):
-        print("I see a sheep")
-        polar(marker = sheep[0])
+def off():
+    R.motors = [0, 0, R.motors[2]]
 
 def angled(e):
     e.bearing.y
     
 def trianglulate():
     global xy, innerAngle
+    clear("wheels")
     while len(i for i in R.see().info.id if i in range(100, 123)) <= 2:
         turn(60)
+        tick()
     wall = R.see().sort(key = angled)
     bigger = [wall[0].bearing.y, wall[0].dist]
     smaller = [wall[1].bearing.y, wall[1].dist]
@@ -147,23 +137,64 @@ def trianglulate():
     xy = [math.cos(angle) * dist - pos[0], math.sin(angle) * dist - pos[1]]
     innerAngle = bigger[0] + angle
 
-def panic():
-    while True:
-        print("How did we get here")
+def scan():
+    R.motors = [0, 0, R.motors[2]]
 
-def front_prox_timeout():
-    global front_prox, seen_front_prox
+    R.see()
+    sheep = R.see()
+    
+    if sheep == []:
+        return turn(60)
+        
 
-    front_prox = False
-    seen_front_prox = False
-    queue("front_prox_timeout()", frontProxTimeout, "proximity")
+    elif sheep[0].info.id in range(40):
+        #priority code for value
+        print("I see a sheep")
+        return polar(marker = sheep[0])
 
+def check():
+    while True:  
+        smallesttask = ["", None, ""]
+        for task in [*tasks.values()]:
+            if smallesttask[1] == None:
+                smallesttask = task
+                continue
+            if task[1] - time.time() + zero < 0 :
+                tasks[task[2]][1] = 10**100
+                continue
+            if task[1] < smallesttask[1]:
+                    smallesttask = task
+    
+        if smallesttask[1] < time.time() - zero + frame:
+            time.sleep(smallesttask[1] - time.time() + zero)
+            eval(smallesttask[0])
+        else:
+            tick()
+        
+threading.Thread(target = check).start()   
+
+while not R.gpio[2].input:
+    tick()
+
+bucketList = [
+    "snap(6, 7)",
+    "scan()",
+    "snap(6,9)"
+]
+
+    
 while True:
-    time.sleep(frame/1000)
+    timer = eval(bucketList[0])
+    while(time.time() - zero < timer):
+        tick()
+        #if collision detection 
+        trianglulate()
+        timer = eval(bucketList[0])
+    bucketList.pop(0)
+    
 
-    check()
 
-    queue("scan()", 0, "wheels")
+
         
     
 

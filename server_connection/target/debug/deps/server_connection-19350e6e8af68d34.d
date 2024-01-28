1d34fe17b31859028d225c01d42f7808f113b8f3
ArchercatNEO <tururu.pompella@gmail.com>
2024-01-28 12:36:25 +0000

    Deletethe rust project

--- server_connection/target/debug/deps/server_connection-19350e6e8af68d34.d
+++
@@ -1,5 +0,0 @@
-/home/ernesto/Dev/RoboCon/server_connection/target/debug/deps/libserver_connection-19350e6e8af68d34.rmeta: src/main.rs
-
-/home/ernesto/Dev/RoboCon/server_connection/target/debug/deps/server_connection-19350e6e8af68d34.d: src/main.rs
-
-src/main.rs:
--- server_connection/target/debug/deps/server_connection-74570c61722cad65.d
+++
@@ -1,5 +0,0 @@
-/home/ernesto/Dev/RoboCon/server_connection/target/debug/deps/libserver_connection-74570c61722cad65.rmeta: src/main.rs
-
-/home/ernesto/Dev/RoboCon/server_connection/target/debug/deps/server_connection-74570c61722cad65.d: src/main.rs
-
-src/main.rs:
